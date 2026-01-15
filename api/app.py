7b218d0c3c803234515c7699b5edb6813726cc6d
VictorRojas3 <vr3hackveneno@gmail.com>
2026-01-14 21:06:42 -0600

    Agregando el esquema de la api

---
+++ api/app.py
@@ -0,0 +1,8 @@
+import requests
+
+data = requests.get('https://app.skiliket.net/server/api/v1/devices/4/devicemeasurements?type=ECO2&start=2026-01-07%2000:00:00&end=2026-01-14%2023:59:59')
+print(data.json())
+nomenclatura_periodo_inicio='2026-01-07%2000:00:00'
+nomenclatura_periodo_final='2026-01-14%2023:59:59'
+url = f'https://app.skiliket.net/server/api/v1/devices/{numero_dispositivo}/devicemeasurements?type={categoria}&start={periodo_inicio}&end={periodo_final}'
+
